931ab92a504f89d6c6652f2715772da330fb1f2a
Amir <mehdizadeh.amir11@gmail.com>
2023-08-14 20:20:09 +0330

    navbar and framer-motion added to Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import './App.css';
 import HomeAnimation from './components/Animate1';
-
+import Navbar from './components/Navbar';
 function App() {
-  return <HomeAnimation />;
+  return (
+    <div className="bg-homePageColor min-h-screen mx-auto p-0">
+      <Navbar />
+      <HomeAnimation />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      homePageColor: "rgb(26, 30, 59)"
+    }
   },
   plugins: [],
 }
