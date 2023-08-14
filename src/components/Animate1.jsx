15af2abf22ad18c1bbdb869a6e4f8ae942491258
Amir <mehdizadeh.amir11@gmail.com>
2023-08-14 19:26:50 +0330

    lottie animation and tailwindcss added to project

---
+++ src/components/Animate1.jsx
@@ -0,0 +1,24 @@
+import Lottie from 'lottie-react';
+import { useEffect, useRef } from 'react';
+import animationData from '../assets/animation_ll9itlkk.json';
+function HomeAnimation() {
+  const lottieRef = useRef();
+  useEffect(() => {
+    if (lottieRef.current) {
+      lottieRef.current.setSpeed(0.7);
+      lottieRef.current.playSegments([10,110] , true)
+    }
+  }, [lottieRef]);
+  return (
+    <div>
+      <Lottie
+        animationData={animationData}
+        loop={true}
+        style={{ width: '300px', height: '300px' }}
+        lottieRef={lottieRef}
+      />
+    </div>
+  );
+}
+
+export default HomeAnimation;
